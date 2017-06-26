c6dfdbefccd63095612e0642eeea37a4a5cab6ba
jason00111 <emberleyjason@gmail.com>
2017-06-26 15:04:06 -0700

    adds routes (wip)

--- app.js
+++ app.js
@@ -10,4 +10,30 @@ app.post('/addToDo', (req, res) => {
   db.addToDo(userId, task) // then redirect to user's todo list page
 })
 
+app.post('/delelteToDo', (req, res) => {
+  const id = req.body.toDoId
+  db.deleteToDoById(id)
+})
+
+app.post('/completeToDo', (req, res) => {
+  const id = req.body.toDoId
+  db.completeToDoById(id)
+})
+
+app.post('/uncompleteToDo', (req, res) => {
+  const id = req.body.toDoId
+  db.uncompleteToDoById(id)
+})
+
+app.post('/editToDo', (req, res) => {
+  const id = req.body.toDoId
+  const task = req.body.task
+  db.editToDoById(id, task)
+})
+
+app.get('/getToDos', (req, res) => {
+  const userId = 1 // get from authentication or session or cookies or something
+  db.getToDosByUserId(userId)
+})
+
 app.listen(3000, () => console.log('listening on port 3000'))
